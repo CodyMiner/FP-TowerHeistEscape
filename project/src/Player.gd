6a84839cb918755a5fae4daec3eee96dd113fd38
Austin <afoster@bsu.edu>
2020-11-05 14:39:07 -0500

    Reformats player scene

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,20 +1,21 @@
 extends KinematicBody2D
 
 signal healh_updated(health)
-signal killed()
+signal _killed()
 
 const GRAVITY := 25
 const JUMP := 650
 const SPEED := 300
 
 export (float) var max_health = 100
+onready var ground_ray := get_node("Ray")
 onready var health = max_health setget _set_health
 onready var invulnerability_timer = $InvulnerabilityTimer
 onready var animation = $Sprite/AnimationPlayer
 onready var animation2 = $Sprite/AnimationPlayer2
 onready var healthlbl = $Health
-
-onready var ground_ray := get_node("Ray")
+onready var hit = $Hit
+onready var jump = $Jump
 
 var velocity := Vector2.ZERO
 
@@ -26,6 +27,7 @@ func _process(delta):
 		velocity.x -= SPEED
 	velocity.y += GRAVITY
 	if Input.is_action_just_pressed("jump") and ground_ray.is_colliding():
+		jump.playing = true
 		velocity.y -= JUMP
 	velocity = move_and_slide(velocity)
 	_set_animation()
@@ -41,7 +43,11 @@ func _set_animation():
 		animation.play("walk")
 	if !(ground_ray.is_colliding()):
 		animation.play("fall")
-		
+
+func _damage_anim():
+	animation2.play("damage")
+	hit.playing = true
+
 func _set_health(value):
 	var prev_health = health
 	health = clamp(value,0,max_health)
@@ -49,18 +55,18 @@ func _set_health(value):
 		emit_signal("healh_updated", health)
 		if health == 0:
 			_kill()
-			emit_signal("killed")
-
-func _kill():
-	var _scene := get_tree().change_scene("res://src/TitleScreen.tscn")
+			emit_signal("_killed()")
 
 func _damage(amount):
 	if invulnerability_timer.is_stopped():
 		invulnerability_timer.start()
-		animation2.play("damage")
+		_damage_anim()
 		_set_health(health-amount)
 		healthlbl.text = String(health)
 
 func _heal(amount):
 	_set_health(health+amount)
 	healthlbl.text = String(health)
+	
+func _kill():
+	var _scene := get_tree().change_scene("res://src/DeathScreen.tscn")
