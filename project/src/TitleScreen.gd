80866773ceccf0aabd4e0394613cad59c65d377f
Austin <afoster@bsu.edu>
2020-11-08 22:41:15 -0500

    Changes to onready var

--- project/src/TitleScreen.gd
+++ project/src/TitleScreen.gd
@@ -1,8 +1,17 @@
 extends Node
 
+onready var music = $ThemeLoop
+onready var player = $Sprites/PlayerSprite/AnimationPlayer
+onready var gem = $Sprites/Gems/AnimationPlayer
+onready var heart = $Sprites/Heart/AnimationPlayer
+onready var saw = $Sprites/Saw/AnimationPlayer
+
 func _ready():
-	$ThemeLoop.playing = true
-	$Sprites/PlayerSprite/AnimationPlayer.play("anim")
+	music.playing = true
+	player.play("anim")
+	gem.play("hover")
+	heart.play("hover")
+	saw.play("rotate")
 
 func _on_Play_pressed():
 	var _scene := get_tree().change_scene("res://src/World.tscn")
